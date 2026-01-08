8a59c72a5a861a122c04ce1a09e04ec2b2d873a8
Nick C <nick@partnersdentalstudio.com>
2026-01-08 15:29:25 -0600

    update filepath for maryam laptop

--- run_MARYAM_local.bat
+++ run_MARYAM_local.bat
@@ -1,7 +1,5 @@
-@echo off
-REM Change to the script's directory (project root)
-REM **FIX MARYAM PAth
-cd /d "C:\Users\MagicTouch\Desktop\Nick\repos\External_Remakes" 
+@echo offREM Change to the script's directory (project root)
+cd /d "C:\Users\Partners\Documents\repos\External_Remakes"
 
 REM Run the Python script using uv
 powershell.exe -Command "uv run python -m src.main"
